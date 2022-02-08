950559d644235f8acb135f0e68a04de953d0abed
Danny <dgimeno777@gmail.com>
2022-02-07 21:22:00 -0500

    add private subnets

--- terraform/vpc/vpc.tf
+++ terraform/vpc/vpc.tf
@@ -1,72 +1,133 @@
-resource "aws_vpc" "terraform_aws_vpc" {
+data "aws_availability_zone" "us_east_1a" {
+  name = "us-east-1a"
+}
+
+data "aws_availability_zone" "us_east_1b" {
+  name = "us-east-1b"
+}
+
+data "aws_availability_zone" "us_east_1c" {
+  name = "us-east-1c"
+}
+
+resource "aws_vpc" "terraform" {
   cidr_block           = "10.0.0.0/16"
   instance_tenancy     = "default"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name : "dgimeno-vpc-${local.resource_name_suffix}"
+    Name : "terraform-${local.resource_name_suffix}"
   }
 }
 
-resource "aws_internet_gateway" "terraform_aws_vpc" {
-  vpc_id = aws_vpc.terraform_aws_vpc.id
+resource "aws_internet_gateway" "terraform" {
+  vpc_id = aws_vpc.terraform.id
   tags = {
-    Name : "dgimeno-igw-${local.resource_name_suffix}"
+    Name : "terraform-${local.resource_name_suffix}"
   }
 }
 
-resource "aws_route_table" "terraform_aws_vpc" {
-  vpc_id = aws_vpc.terraform_aws_vpc.id
+resource "aws_route_table" "terraform_public" {
+  vpc_id = aws_vpc.terraform.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.terraform_aws_vpc.id
+    gateway_id = aws_internet_gateway.terraform.id
   }
   tags = {
-    Name : "dgimeno-route-table-${local.resource_name_suffix}"
+    Name : "terraform-public-${local.resource_name_suffix}"
   }
 }
 
-resource "aws_subnet" "terraform_aws_vpc_public_a" {
-  vpc_id                  = aws_vpc.terraform_aws_vpc.id
+resource "aws_route_table" "terraform_private" {
+  vpc_id = aws_vpc.terraform.id
+  tags = {
+    Name : "terraform-private-${local.resource_name_suffix}"
+  }
+}
+
+resource "aws_subnet" "terraform_public_a" {
+  vpc_id                  = aws_vpc.terraform.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = data.aws_availability_zone.us_east_1a.name
   map_public_ip_on_launch = true
   tags = {
-    Name : "dgimeno-subnet-1a-${local.resource_name_suffix}"
+    Name : "terraform-public-1a-${local.resource_name_suffix}"
   }
 }
 
-resource "aws_route_table_association" "terraform_aws_vpc_public_a" {
-  route_table_id = aws_route_table.terraform_aws_vpc.id
-  subnet_id      = aws_subnet.terraform_aws_vpc_public_a.id
+resource "aws_route_table_association" "terraform_public_a" {
+  route_table_id = aws_route_table.terraform_public.id
+  subnet_id      = aws_subnet.terraform_public_a.id
 }
 
-resource "aws_subnet" "terraform_aws_vpc_public_b" {
-  vpc_id                  = aws_vpc.terraform_aws_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "terraform_private_a" {
+  vpc_id            = aws_vpc.terraform.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zone.us_east_1a.name
   tags = {
-    Name : "dgimeno-subnet-1b-${local.resource_name_suffix}"
+    Name : "terraform-private-1a-${local.resource_name_suffix}"
   }
 }
 
-resource "aws_route_table_association" "terraform_aws_vpc_public_b" {
-  route_table_id = aws_route_table.terraform_aws_vpc.id
-  subnet_id      = aws_subnet.terraform_aws_vpc_public_b.id
+resource "aws_route_table_association" "terraform_private_a" {
+  route_table_id = aws_route_table.terraform_private.id
+  subnet_id      = aws_subnet.terraform_private_a.id
 }
 
-resource "aws_subnet" "terraform_aws_vpc_public_c" {
-  vpc_id                  = aws_vpc.terraform_aws_vpc.id
+resource "aws_subnet" "terraform_public_b" {
+  vpc_id                  = aws_vpc.terraform.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-1c"
+  availability_zone       = data.aws_availability_zone.us_east_1b.name
   map_public_ip_on_launch = true
   tags = {
-    Name : "dgimeno-subnet-1c-${local.resource_name_suffix}"
+    Name : "terraform-public-1b-${local.resource_name_suffix}"
   }
 }
 
-resource "aws_route_table_association" "terraform_aws_vpc_public_c" {
-  route_table_id = aws_route_table.terraform_aws_vpc.id
-  subnet_id      = aws_subnet.terraform_aws_vpc_public_c.id
+resource "aws_route_table_association" "terraform_public_b" {
+  route_table_id = aws_route_table.terraform_public.id
+  subnet_id      = aws_subnet.terraform_public_b.id
+}
+
+resource "aws_subnet" "terraform_private_b" {
+  vpc_id            = aws_vpc.terraform.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = data.aws_availability_zone.us_east_1b.name
+  tags = {
+    Name : "terraform-private-1b-${local.resource_name_suffix}"
+  }
+}
+
+resource "aws_route_table_association" "terraform_private_b" {
+  route_table_id = aws_route_table.terraform_private.id
+  subnet_id      = aws_subnet.terraform_private_b.id
+}
+
+resource "aws_subnet" "terraform_public_c" {
+  vpc_id                  = aws_vpc.terraform.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = data.aws_availability_zone.us_east_1c.name
+  map_public_ip_on_launch = true
+  tags = {
+    Name : "terraform-public-1c-${local.resource_name_suffix}"
+  }
+}
+
+resource "aws_route_table_association" "terraform_public_c" {
+  route_table_id = aws_route_table.terraform_public.id
+  subnet_id      = aws_subnet.terraform_public_c.id
+}
+
+resource "aws_subnet" "terraform_private_c" {
+  vpc_id            = aws_vpc.terraform.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = data.aws_availability_zone.us_east_1c.name
+  tags = {
+    Name : "terraform-private-1c-${local.resource_name_suffix}"
+  }
+}
+
+resource "aws_route_table_association" "terraform_private_c" {
+  route_table_id = aws_route_table.terraform_private.id
+  subnet_id      = aws_subnet.terraform_private_c.id
 }
